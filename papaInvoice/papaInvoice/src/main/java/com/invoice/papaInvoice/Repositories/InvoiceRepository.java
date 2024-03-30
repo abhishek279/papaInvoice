43ca9238d406aefe22173a740fe172c3aa2afb28
abhishek279 <32049901+abhishek279@users.noreply.github.com>
2024-03-30 10:17:20 -0400

    Delete papaInvoice/papaInvoice directory

--- papaInvoice/papaInvoice/src/main/java/com/invoice/papaInvoice/Repositories/InvoiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.invoice.papaInvoice.Repositories;
-
-
-import com.invoice.papaInvoice.Entity.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-}
--- papaInvoice/papaInvoice/src/main/java/com/invoice/papaInvoice/Repositories/PerFormaInvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.invoice.papaInvoice.Repositories;
-
-
-import com.invoice.papaInvoice.Entity.Invoice;
-import com.invoice.papaInvoice.Entity.ProformaInvoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerFormaInvoiceRepository extends JpaRepository<ProformaInvoice, Long> {
-}
